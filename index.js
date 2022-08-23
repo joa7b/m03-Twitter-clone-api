bd03a5f4dc815f8f2bb58a6977ccf975c112d81c
joa7b <joa7@protonmail.com>
2022-08-23 05:53:15 -0300

    first commit

---
+++ index.js
@@ -0,0 +1,12 @@
+require("dotenv").config();
+const express = require("express");
+const cors = require("cors");
+
+const port = process.env.PORT || 3001;
+const app = express();
+
+app.use(cors());
+
+app.listen(port, () => {
+    console.log(`Servidor rodando na porta ${port}`);
+});
