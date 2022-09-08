5d13424df1727b3857e67eaaebccd3f5fabea2eb
joa7b <joa7@protonmail.com>
2022-09-08 06:19:50 -0300

    bcrypt

--- src/users/User.js
+++ src/users/User.js
@@ -1,4 +1,5 @@
 const mongoose = require("mongoose");
+const bcrypt = require("bcryptjs");
 
 const UserSchema = new mongoose.Schema({
   name: {
@@ -26,6 +27,12 @@ const UserSchema = new mongoose.Schema({
     required: true,
   },
 });
+
+UserSchema.pre("save", async function (next) {
+  this.password = await bcrypt.hash(this.password, 10);
+
+  next();
+})
 
 const User = mongoose.model("User", UserSchema);
 
